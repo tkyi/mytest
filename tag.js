437d5971a42f212ca1036bf17acef44bf2f1845d
Tiffany K <tiffanykyi@gmail.com>
2017-07-26 15:34:57 -0700

    Update tag.js

--- tag.js
+++ tag.js
@@ -4,14 +4,14 @@ var request = require('request');
 
 const params = {
     body: {
-          version: '1.0.1'
+          version: '1.0.2'
       },
       json: true,
       auth: {
           bearer: process.env.SD_TOKEN
       },
       method: 'PUT',
-      url: `https://api.screwdriver.cd/v4/templates/tifftemplate/tags/latest`
+      url: `https://api.screwdriver.cd/v4/templates/tifftemplate/tags/petey`
   };
 
 return new Promise((resolve, reject) => {
