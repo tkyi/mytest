6afe2558eb9056854e202cf37708cac5793e42a9
Tiffany K <tiffanykyi@gmail.com>
2017-07-26 11:21:44 -0700

    Create tag.js

---
+++ tag.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var request = require('request');
+
+const params = {
+    body: {
+          version: '1.0.0'
+      },
+      json: true,
+      auth: {
+          bearer: process.env.SD_TOKEN
+      },
+      method: 'PUT',
+      url: `https://api.screwdriver.cd/v4/templates/tifftemplate/tags/stable`
+  };
+
+return new Promise((resolve, reject) => {
+    request(params, (err, response, body) => {
+        console.log('here');
+        if (err) {
+            console.log('err', err);
+
+            return reject(err);
+        }
+
+        console.log(response.statusCode);
+
+        console.log('****')
+        console.log(response.body);
+        return resolve(body);
+      })
+});
